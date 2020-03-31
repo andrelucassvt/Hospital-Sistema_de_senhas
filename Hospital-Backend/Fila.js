28bec8cb4c6c68d58c222ffaae9d0d480dc4a34c
Diogo L. C. Felipe <dlcfelipp@gmail.com>
2020-03-31 17:42:03 -0300

    Estrutura de dados

---
+++ Hospital-Backend/Fila.js
@@ -0,0 +1,56 @@
+class NoFila{
+    constructor(valor, proximo){
+        this.valor = valor;
+        this.proximo = proximo;
+    }
+}
+
+class Fila{
+    constructor(){
+        this.senhaAtual = 1;
+        this.cabeça = null;
+        this.calda = null;
+    }
+
+    add(){
+        let novoNo = new NoFila(this.senhaAtual, null);
+
+        this.senhaAtual++;
+
+        if(this.cabeça == null){
+            this.cabeça = novoNo;
+            this.calda = novoNo;
+        } else {
+            this.calda.proximo = novoNo;
+            this.calda = novoNo;
+        }
+
+        return novoNo.valor;
+    }
+
+    remove(){
+        let senhaRemovida = this.cabeça;
+        if(this.cabeça == null){
+            console.log("Fila Vazia");
+            return "Fila Vazia";
+        } else {
+            this.cabeça = this.cabeça.proximo;
+        }
+        return senhaRemovida.valor;
+    }
+
+    ler(){
+        let noAtual = this.cabeça;
+        console.log("Lendo Fila")
+        while(noAtual != null){
+            console.log(noAtual.valor);
+        }
+    }
+
+    esvaziar(){
+        console.log("Evaziando");
+        this.cabeça = null;
+    }
+}
+
+module.exports = Fila;
