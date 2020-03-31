474cb567c61ce23b21d964af4e3ac3fea480bd26
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-31 15:09:41 -0300

    Tela de cadastro do funcionario adicionado

--- Hospital/src/hospital/TelaPrincipal.java
+++ Hospital/src/hospital/TelaPrincipal.java
@@ -299,11 +299,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here:
         
-        /*
+        
         TelaCadastroFuncionario TelaCadUser = new TelaCadastroFuncionario();
         PanePrincipal.add(TelaCadUser);
         TelaCadUser.setVisible(true);
-        */
+        
         
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
